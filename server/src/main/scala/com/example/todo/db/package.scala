232bf535bf9fe91fb58f2a4871f17620df4ea812
alex <alex@eldis.ru>
2022-04-11 16:01:44 +0700

    Doobie second programm - begin

--- server/src/main/scala/com/example/todo/db/package.scala
+++ server/src/main/scala/com/example/todo/db/package.scala
@@ -1,6 +1,6 @@
 package com.example.db
 
-import cats.effect._, cats.implicits._
+import cats.effect._
 import doobie._, doobie.implicits._
 
 // TODO This is just for testing. Consider using cats.effect.IOApp instead of calling
@@ -10,14 +10,16 @@ import cats.effect.unsafe.implicits.global
 // TODO A transactor that gets connections from java.sql.DriverManager and executes blocking operations
 // on an our synchronous EC. See the chapter on connection handling for more info.
 package object db {
-  val xa = Transactor.fromDriverManager[IO](
-    "org.postgresql.Driver", // driver classname
-    "jdbc:postgresql:world", // connect URL (driver-specific)
-    "postgres", // user
-    "" // password
-  )
+  def connect() = {
+    val xa = Transactor.fromDriverManager[IO](
+      "org.postgresql.Driver", // driver classname
+      "jdbc:postgresql://postgres/todo", // connect URL (driver-specific)
+      "postgres", // user
+      "1111" // password
+    )
 
-  val program1 = 42.pure[ConnectionIO]
-  val io = program1.transact(xa)
-  io.unsafeRunSync()
+    val program1 = sql"select 42".query[Int].unique
+    val io = program1.transact(xa)
+    io.unsafeRunSync()
+  }
 }
